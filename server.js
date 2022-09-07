5127be6b40d76aa14f5ceec04269262148801e38
kktrading <kktrading@gmail.com>
2022-09-07 09:10:26 +0530

    d

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://kktrading.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://kktradingweb.web.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
