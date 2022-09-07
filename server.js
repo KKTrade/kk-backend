813471469c2dad97cac94087721e26f45e2f71d7
kktrading <kktrading@gmail.com>
2022-09-07 13:29:26 +0530

    leaving

--- server.js
+++ server.js
@@ -32,12 +32,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.options("/api/*", cors(), (req, res) => {
   res.sendStatus(204);
 });
